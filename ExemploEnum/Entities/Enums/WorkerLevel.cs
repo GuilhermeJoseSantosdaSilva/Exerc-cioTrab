5ab6313b8a609ff3012cb0402be6e228c45ccfdc
GuilhermeJoseSantosdaSilva <gujose00@gmail.com>
2021-01-20 12:34:52 -0300

    Adicionando todas as classes e Enum

---
+++ ExemploEnum/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExemploEnum.Entities.Enums
+{
+    enum WorkerLevel
+    {
+        JUNIOR = 0,
+        MIDLEVEL = 1,
+        SENIOR = 2
+    }
+}
